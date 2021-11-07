2eb1f33eb34d0ac97c7978470df73c2410fcd773
Patrick Eriksson <paleite@users.noreply.github.com>
2021-11-07 02:13:28 +0100

    Fix voice bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,43 @@
-import React from "react";
+import * as React from "react";
 
-function App() {
+const getVoice = (voicesList: SpeechSynthesisVoice[]): SpeechSynthesisVoice => {
+  return voicesList
+    .sort((a, b) =>
+      a.localService !== b.localService ? (a.localService !== true ? 1 : -1) : 0
+    )
+    .sort((a, b) => (a.lang !== b.lang ? (a.lang !== "de-DE" ? 1 : -1) : 0))
+    .reduce<SpeechSynthesisVoice>(
+      (acc, cur) => (cur.lang === "de-DE" ? cur : acc),
+      voicesList[0]
+    );
+};
+
+const App: React.FunctionComponent = () => {
   const [letter, setLetter] = React.useState("");
-  // const [voices, setVoices] = React.useState(null);
+  const [voicesList, setVoicesList] = React.useState<SpeechSynthesisVoice[]>(
+    window.speechSynthesis.getVoices()
+  );
+  const voice = React.useMemo(() => getVoice(voicesList), [voicesList]);
 
-  // React.useEffect(() => {
-  //   var speech = new SpeechSynthesisUtterance(letter);
-  //   const synth = window.speechSynthesis;
-  //   let voices = synth.getVoices();
-  //   let voice = voices.find(item => item.lang === "de-DE");
+  window.speechSynthesis.onvoiceschanged = () =>
+    setVoicesList(() => window.speechSynthesis.getVoices());
 
-  //   console.log(voices, voice);
-  //   speech.voice = voice;
-  //   // console.dir(utterThis);
-  //   // console.log(utterThis, utterThis.voice);
-  //   synth.cancel();
-  //   synth.speak(speech);
-  // }, [letter, setLetter]);
-
-  const handleChange = (e: $TSFixMe) => {
-    console.log("triggererd");
-    const nextLetter = e.target.value.substr(-1).toUpperCase();
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    // const nextLetter = event.target.value.slice(-1).toUpperCase();
+    const nextLetter = event.target.value.split(" ").slice(-1).join("");
     setLetter(nextLetter);
 
     ((spokenLetter) => {
       const speech = new SpeechSynthesisUtterance(spokenLetter);
-      const synth = window.speechSynthesis;
-      const voices = synth.getVoices();
-      const voice = voices.filter((item) => item.lang === "de-DE").pop();
-
-      console.log(voices, voice);
-      // @ts-expect-error ts-migrate(2322) FIXME: Type 'SpeechSynthesisVoice | undefined' is not ass... Remove this comment to see the full error message
       speech.voice = voice;
-      // console.dir(utterThis);
-      // console.log(utterThis, utterThis.voice);
-      synth.cancel();
-      synth.speak(speech);
+
+      window.speechSynthesis.cancel();
+      window.speechSynthesis.speak(speech);
     })(nextLetter);
   };
 
   return (
     <div className="App">
-      // @ts-expect-error ts-migrate(7026) FIXME: JSX element implicitly has
-      type 'any' because no i... Remove this comment to see the full error
-      message
-      // @ts-expect-error ts-migrate(7026) FIXME: JSX element implicitly has type 'any' because no i... Remove this comment to see the full error message
       <input
         type="text"
         name="letter"
@@ -57,12 +50,8 @@ function App() {
           textAlign: "center",
         }}
       />
-      // @ts-expect-error ts-migrate(7026) FIXME: JSX element implicitly has
-      type 'any' because no i... Remove this comment to see the full error
-      message
-    // @ts-expect-error ts-migrate(7026) FIXME: JSX element implicitly has type 'any' because no i... Remove this comment to see the full error message
     </div>
   );
-}
+};
 
 export default App;
